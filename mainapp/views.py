5efd99622873a2649a0796b69a09f2a18199609f
Jonathan <jnavarrete@k2ins.com>
2019-08-12 14:48:05 -0700

    adding templates to view

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .forms import UserInfoForm, LanguagesFormSet, SportsFormSet
 
-# Create your views here.
+def home(request):
+    return render(request, 'mainapp/home.html')
+
+def newapp(request):
+    context = {'userinfoform': UserInfoForm}
+    return render(request, 'mainapp/newapp.html', context)
+
+
+def appform(request, userid):
+    context = {'userinfoform': UserInfoForm, 'languagesform': LanguagesFormSet, 'sportsform': SportsFormSet}
+    return render(request, 'mainapp/<userid>/appform.html', context)
