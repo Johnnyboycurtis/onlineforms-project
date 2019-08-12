f3bd8a1a5f2a5c8438add58e128a3665db2335e5
Jonathan <jnavarrete@k2ins.com>
2019-08-12 13:47:04 -0700

    adding models

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import UserInfo, ProgrammingLanguages, Sports
+
 # Register your models here.
+admin.site.Register(UserInfo)
+admin.site.Register(ProgrammingLanguages)
+admin.site.Register(Sports)
