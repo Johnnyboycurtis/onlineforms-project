c5f8956c1118abbf8f771df0c5a90aa91c6d1fe5
Jonathan <jnavarrete@k2ins.com>
2019-08-13 13:00:43 -0700

    user registration forms

--- users/views.py
+++ users/views.py
@@ -1,4 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+
+from .forms import UserRegisterForm
 
 def register(request):
-    return render(request, 'users/register.html')
+    if request.method == "POST":
+        #form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            form.save()
+            messages.success(request, "User {} created!".format(username))
+            return redirect("home")
+    else:
+        form = UserRegisterForm()
+    context = {'form': form}
+    return render(request, 'users/register.html', context)
