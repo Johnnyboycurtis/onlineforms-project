afb016bd1c55b90189b4bb7eee10e7320a0d5983
Jonathan <jnavarrete@k2ins.com>
2019-08-13 10:36:01 -0700

    reverted back to uuid.uuid4

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,8 +1,5 @@
 from django.db import models
-
-# Create your models here.
-
-from django.db import models
+import uuid
 
 import random
 
@@ -12,7 +9,7 @@ def rand():
 SKILL_CHOICES = [('Beginner', 'Beginner'), ('Moderate', 'Moderate'), ('Expert', 'Expert')]
 
 class UserInfo(models.Model):
-    userid = models.UUIDField(primary_key=True, default='00{}'.format(rand()), editable=False)
+    userid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length = 100)
     email = models.CharField(max_length = 100, default='something-{}@gmail.com'.format(rand()))
     age = models.IntegerField(default = random.randint(a=22, b=45))
