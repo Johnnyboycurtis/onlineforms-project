6a11962ea4ceef93845d80000e3838a8c456eb96
Jonathan <jnavarrete@k2ins.com>
2019-08-14 12:58:55 -0700

    added a method to add user info to form and save

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from .forms import ContactsForm, LanguagesFormSet, SportsFormSet
 from django.contrib import messages
 
@@ -8,14 +9,16 @@ def home(request):
 def about(request):
     return render(request, 'mainapp/about.html')
 
+
+@login_required
 def newapp(request):
     context = {'userinfoform': ContactsForm}
     if request.method == "POST":
-        #print(request.POST)
         form = ContactsForm(request.POST)
-        print(form)
         if form.is_valid():
-            form.save()
+            instance = form.save(commit = False) # https://www.youtube.com/watch?v=2h57cqFRcqg
+            instance.user = request.user
+            instance.save()
             messages.success(request, "Saved new contact!")
             return redirect('home') #render(request, 'mainapp/home.html', {'form': form})
     return render(request, 'mainapp/newapp.html', context)
@@ -24,26 +27,3 @@ def newapp(request):
 def appform(request):
     return render(request, 'mainapp/home.html')
 
-#def appform(request, userid):
-#    context = {'userinfoform': ContactsForm, 'languagesform': LanguagesFormSet, 'sportsform': SportsFormSet}
-#    return render(request, 'mainapp/<userid>/appform.html', context)
-
-"""
-def edit_appform(request, userid):
-    # if this is a POST request we need to process the form data
-    if request.method == 'POST':
-        # create a form instance and populate it with data from the request:
-        form = NameForm(request.POST)
-        # check whether it's valid:
-        if form.is_valid():
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
-            return HttpResponseRedirect('/thanks/')
-
-    # if a GET (or any other method) we'll create a blank form
-    else:
-        form = NameForm()
-
-    return render(request, 'name.html', {'form': form})
-"""
