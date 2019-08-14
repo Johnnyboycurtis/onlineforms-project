fd8bd66bf8abe9b7f75fb73be85fc16218ec8cf9
Jonathan <jnavarrete@k2ins.com>
2019-08-14 12:57:56 -0700

    removed dummy model

--- mainapp/models.py
+++ mainapp/models.py
@@ -9,15 +9,6 @@ def rand():
 
 SKILL_CHOICES = [('Beginner', 'Beginner'), ('Moderate', 'Moderate'), ('Expert', 'Expert')]
 
-
-class Dummy(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length = 100)
-    age = models.CharField(max_length = 100)
-    
-    class Meta:
-        verbose_name_plural = "Dummy"
-
 class Contacts(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length = 100)
@@ -36,7 +27,6 @@ class ProgrammingLanguages(models.Model):
         verbose_name_plural = "ProgrammingLanguages"
 
 class Sports(models.Model):
-    #userid = models.ForeignKey('ContactInfo', on_delete=models.CASCADE)
     contact = models.ForeignKey(Contacts, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     skill = models.CharField(max_length=100, choices = SKILL_CHOICES)
