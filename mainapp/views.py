28d296cf13cb4a6efbc992840921734df8cea57d
Jonathan <jnavarrete@k2ins.com>
2019-08-14 14:58:56 -0700

    added titles to context

--- mainapp/views.py
+++ mainapp/views.py
@@ -3,11 +3,20 @@ from django.contrib.auth.decorators import login_required
 from .forms import ContactsForm, LanguagesFormSet, SportsFormSet
 from django.contrib import messages
 
+from .models import Contacts
+
 def home(request):
-    return render(request, 'mainapp/home.html')
+    context = {'title': 'Home'}
+    if request.method == "GET" and request.user.is_authenticated:
+        print("print out the user's contacts now")
+        context['contacts'] = Contacts.objects.all()
+        return render(request, 'mainapp/home.html', context)
+
+    return render(request, 'mainapp/home.html', context)
 
 def about(request):
-    return render(request, 'mainapp/about.html')
+    context = {'title': 'About'}
+    return render(request, 'mainapp/about.html', context)
 
 
 @login_required
