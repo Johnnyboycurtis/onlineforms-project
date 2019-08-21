f25a0bed1c439947716bbfe95b068014589a1115
Jonathan <johnnyboycurtis@gmail.com>
2019-08-21 13:48:01 -0700

    register models

--- billing/admin.py
+++ billing/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Company, BillHeader, BillLines
 
-# Register your models here.
+
+admin.site.register(Company)
+admin.site.register(BillHeader)
+admin.site.register(BillLines)
