61c6497cb65b0b11be6b4cd96c589f4fdd73fbe5
jnavarrete@k2ins.com <jnavarrete@k2ins.com>
2019-08-22 14:32:58 -0700

    added edit url

--- billing/urls.py
+++ billing/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('create/', views.BillHeaderCreate.as_view(), name = 'createinvoice'),
-    path('edit/<int:pk>', views.UpdateBillHeader.as_view(), name = 'editinvoice'),
+    path('<int:pk>/editbillheader/', views.UpdateBillHeader.as_view(), name = 'editbillheader'),
+    path('<int:pk>/edit/', views.UpdateInvoice.as_view(), name = 'editinvoice'),
     path('', views.BillListView.as_view(), name = 'billinghome')
 ]
