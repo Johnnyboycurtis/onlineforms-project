98da719e7611933ff9401840f88e2ae26d0915a7
jonathan <johnnyboycurtis@gmail.com>
2019-08-25 16:33:23 -0700

    resized Textarea widget

--- billing/forms.py
+++ billing/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm, inlineformset_factory
+from django.forms import ModelForm, inlineformset_factory, Textarea
 from .models import BillHeader, BillLines
 
 
@@ -7,4 +7,14 @@ class BillHeaderForm(ModelForm):
         model = BillHeader
         fields = ['company_name', 'street_address', 'city', 'state', 'phone', 'email']
 
-BillLineFormSet = inlineformset_factory(BillHeader, BillLines, fields = ('description', 'quantity', 'unit_price'))
+
+class BillLineForm(ModelForm):
+    class Meta:
+        model = BillLines
+        fields = ('description', 'quantity', 'unit_price')
+        widgets = {
+          "description": Textarea(attrs={'rows':2, 'cols':20}),
+        }
+
+
+BillLineFormSet = inlineformset_factory(BillHeader, BillLines, fields = ('description', 'quantity', 'unit_price'), form = BillLineForm)
